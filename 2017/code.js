6846f9f7d90ea2e1dc3261e14a87992e77973445
Chris Richards <ripter001@gmail.com>
2017-02-04 01:14:39 -0800

    763 byte is 729 with new compression

--- 2017/code.js
+++ 2017/code.js
@@ -1,5 +1,4 @@
 function createSVG(polygons) {
-  const div = document.createElement('div');
   const renderedPolygons = polygons.map(function(polygon) {
     return [
       '<polygon points="',
@@ -10,20 +9,17 @@ function createSVG(polygons) {
     ].join('');
   }).join('');
 
-  div.innerHTML = [
+  return [
     '<svg viewBox="0 0 100 100">',
     renderedPolygons,
     '</svg>'].join('');
-  return div;
 }
 
-function createCSS(elm) {
-  const style = document.createElement('style');
-  style.innerHTML = 'svg {height:300px; width:300px;}svg polygon {stroke: #001f3f;}'
-  elm.append(style);
-  return style;
+function add(tag, innerHTML, parent) {
+  var elm = document.createElement(tag);
+  elm.innerHTML = innerHTML;
+  return elm;
 }
-
 
 window.active = null;
 window.magic = function magic(evt) {
@@ -43,11 +39,11 @@ window.magic = function magic(evt) {
   }
 };
 
-var tree = createSVG([
+var tree = add('div', createSVG([
   ['40,50 60,50 60,100 40,100', 'fill: brown;'],
   ['25,25 75,25 75,75, 25,75', 'fill: #2ECC40;'],
   ['18,19 18,44 43,44, 43,19', 'fill: #2ECC40;'],
   ['60,47 60,72 85,72, 85,47', 'fill: #2ECC40;'],
-]);
-createCSS(tree);
+]));
+tree.append(add('style', 'svg {height:300px; width:300px;}svg polygon {stroke: #001f3f;}'));
 b.prepend(tree);
