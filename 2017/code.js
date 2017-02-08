9d4f0996da31745acbbd9e933f9855783e4d6cf3
Chris Richards <ripter001@gmail.com>
2017-02-07 22:11:26 -0800

    added babili to use es6

--- 2017/code.js
+++ 2017/code.js
@@ -10,6 +10,9 @@ function createSVG(polygons) {
     +'</svg>';
 }
 
+/**
+ * Creates tag with html prepend to the parent.
+ */
 function add(tag, innerHTML, parent) {
   var elm = document.createElement(tag);
   elm.innerHTML = innerHTML;
@@ -17,16 +20,23 @@ function add(tag, innerHTML, parent) {
   return elm;
 }
 
+
 window.active = null;
 window.m = function magic(evt) {
+  var target = evt.target;
+
   switch (evt.type) {
     case 'mousedown':
       window.active = [evt.x, evt.y];
+      target.deltaX = evt.x;
+      target.deltaY = evt.y;
       break;
     case 'mousemove':
       if (!window.active) { return; }
-      const deltaX = evt.x - active[0];
-      const deltaY = evt.y - active[1];
+      // const deltaX = evt.x - active[0];
+      // const deltaY = evt.y - active[1];
+      var deltaX = evt.x - target.deltaX;
+      var deltaY = evt.y - target.deltaY;
       evt.target.setAttribute('transform', 'translate('+deltaX+', '+deltaY+')');
       break;
     case 'mouseup':
@@ -35,7 +45,7 @@ window.m = function magic(evt) {
   }
 };
 
-var elmSvg = add('div', createSVG([
+var tangram = [
   ['0,0 0,100 50,50', 'fill: #001f3f'],
   ['0,100 100,100 50,50', 'fill: #0074D9'],
   ['100,100 100,50 75,75', 'fill: #7FDBFF'],
@@ -43,5 +53,16 @@ var elmSvg = add('div', createSVG([
   ['50,50 75,25 100,50 75,75', 'fill: #3D9970'],
   ['50,50 75,25 25,25', 'fill: #2ECC40'],
   ['0,0 50,0 75,25 25,25', 'fill: #01FF70'],
-]), b);
-add('style', 'svg {height:300px; width:300px;}svg polygon {stroke: #001f3f;}', elmSvg);
+];
+
+var SCALE = 0.25;
+tangram = tangram.map(function(polygon) {
+  polygon[0] = polygon[0].replace(/(\d+),(\d+)/g, function(point, x, y) {
+    return ''+ x*SCALE +','+ y*SCALE;
+  });
+  // polygon[0] = polygon[0].replace(/(\d+)/g, (x) => x*SCALE);
+  return polygon;
+});
+
+var elmSvg = add('div', createSVG(tangram), b);
+// add('style', 'svg {height:300px; width:300px;}svg polygon {stroke: #001f3f;}', elmSvg);
--- 2017/rename.sh
+++ 2017/rename.sh
@@ -2,6 +2,10 @@
 FILE_SIZE=$(stat -f%z ./code_rp.js)
 echo SIZE: $FILE_SIZE
 # echo ./$FILE_SIZE\_code.js
-cp ./code.js ./$FILE_SIZE\_code.js
-mv ./code_c.js ./$FILE_SIZE\_code_c.js
-mv ./code_rp.js ./$FILE_SIZE\_code_rp.js
+cp ./code.js versions/$FILE_SIZE\_code.js
+# babili
+mv ./code_babili.js versions/FILE_SIZE\_code_babili.js
+mv ./code_babili_rp.js versions/FILE_SIZE\_code_babili_rp.js
+# uglify
+mv ./code_uglify.js versions/FILE_SIZE\_code_uglify.js
+mv ./code_uglify_rp.js versions/FILE_SIZE\_code_uglify_rp.js
