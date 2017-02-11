254de0272e35f765577006253d23edc8f3dcd7ca
Chris Richards <ripter001@gmail.com>
2017-02-10 19:53:36 -0800

    drag and drop is working

--- 2017/rename.sh
+++ 2017/rename.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-FILE_SIZE=$(stat -f%z ./code_rp.js)
+FILE_SIZE=$(stat -f%z ./code_uglify_rp.js)
 echo SIZE: $FILE_SIZE
 # echo ./$FILE_SIZE\_code.js
 cp ./code.js versions/$FILE_SIZE\_code.js
 # babili
-mv ./code_babili.js versions/FILE_SIZE\_code_babili.js
-mv ./code_babili_rp.js versions/FILE_SIZE\_code_babili_rp.js
+mv ./code_babili.js versions/$FILE_SIZE\_code_babili.js
+mv ./code_babili_rp.js versions/$FILE_SIZE\_code_babili_rp.js
 # uglify
-mv ./code_uglify.js versions/FILE_SIZE\_code_uglify.js
-mv ./code_uglify_rp.js versions/FILE_SIZE\_code_uglify_rp.js
+mv ./code_uglify.js versions/$FILE_SIZE\_code_uglify.js
+mv ./code_uglify_rp.js versions/$FILE_SIZE\_code_uglify_rp.js
