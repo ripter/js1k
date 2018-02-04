8d9a0e66375de403cb3fba980a5a948f25e54baa
Chris Richards <ripter001@gmail.com>
2018-02-03 18:45:34 -0800

    Trying to figure out projection

--- 2018/index.js
+++ 2018/index.js
@@ -1,5 +1,6 @@
 console.log('Coin Miner 2018');
 // colors: http://www.color-hex.com/color-palette/43493
+// projection: https://www.sitepoint.com/building-3d-engine-javascript/
 //
 //TEMP
 const elRefrence = document.createElement('img');
@@ -39,19 +40,28 @@ function tick(timestamp = 0) {
     // clearScreen();
     c.drawImage(elRefrence, -159, 0);
 
-    // drawCross();
-    // drawBox(1, (WIDTH/2), 10, 100, 100);
-    // drawBox(5, (WIDTH/2), 200, 100, 100);
-
     c.strokeStyle = 'red';
-    drawVanishing(0);
-    c.strokeStyle = 'blue';
-    drawVanishing(1);
-    c.strokeStyle = 'green';
-    drawVanishing(2);
-    c.strokeStyle = 'orange';
-    drawVanishing(3);
+    drawVert(0);
+    drawVert(1);
+    drawVert(2);
+    drawVert(3);
+    drawVert(4);
+    drawVert(5);
+    drawVert(6);
 
+    c.strokeStyle = 'green';
+    drawHorz(0);
+    drawHorz(1);
+    drawHorz(2);
+    drawHorz(3);
+    drawHorz(4);
+    drawHorz(5);
+    drawHorz(6);
+
+
+    // c.clearRect(0, 0, WIDTH, (HEIGHT/4)*1.5);
+
+    // outlike the UI
     c.strokeStyle = 'black';
     c.strokeRect(0, 0, WIDTH, HEIGHT);
   }
@@ -82,79 +92,42 @@ function lineAtAngle(x1, y1, length, angle) {
   c.lineTo(x2, y2);
 }
 
-function drawCross() {
-  c.strokeStyle = '#392b1b';
-  c.save();
-  c.beginPath();
 
-  c.moveTo(WIDTH/2, 0);
-  c.lineTo(WIDTH/2, HEIGHT);
-  c.moveTo(0, HEIGHT/2);
-  c.lineTo(WIDTH, HEIGHT/2);
-
-  c.closePath();
-  c.stroke();
-}
-
-function drawVanishing(distance = 0) {
+function drawVert(distance = 0) {
   const degree = (90 * distance);
-  console.log('degree', degree);
+  const size = 8.6;
   c.beginPath();
 
-  // lineAtAngle(VPOINT.X, VPOINT.Y, WIDTH/2, ((15 * distance) * Math.PI / 180));
-  lineAtAngle(VPOINT.X, VPOINT.Y, WIDTH, degree);
-  // c.moveTo(VPOINT.X, VPOINT.Y);
-  // c.lineTo(0, WIDTH + (size * distance));
-  // c.lineTo(VPOINT.X - (size * distance), VPOINT.Y + 200);
-  // c.moveTo(VPOINT.X, VPOINT.Y);
-  // c.lineTo(WIDTH, WIDTH + (size * distance));
+  lineAtAngle(VPOINT.X, VPOINT.Y, WIDTH, 90 - (size * distance));
+  lineAtAngle(VPOINT.X, VPOINT.Y, WIDTH, 90 + (size * distance));
 
   c.closePath();
   c.stroke();
 }
 
-
-// draw perpective box that lays flat on the x-axis
-// Instead of the position being the top,left, it's the center of the box.
-function drawBox(z, x, y, width, height) {
-  c.fillStyle = '#f0f0b5';
-  c.strokeStyle = '#392b1b';
-  const perpective = 2/3;
-  const HALF_WIDTH = width/2;
-  const HALF_HEIGHT = height/2;
-  const OFFSET = 0| z * perpective;
-  let x1 = (x - HALF_WIDTH) - OFFSET;
-  let y1 = y;
-  let x2 = (x + HALF_WIDTH);// / z;
-  let y2 = y;
-  let x3 = (x + HALF_WIDTH);// / z;
-  let y3 = y + height;
-  let x4 = (x - HALF_WIDTH);// * z;
-  let y4 = y + height;
-  c.save();
+function drawHorz(distance = 0) {
+  // const size = (8.6 * distance);
+  const size = 10;
+  const FLOOR = 275;
+  const offset = (distance * size);
   c.beginPath();
 
-  console.group('drawBox')
-  console.log('z', z);
-  console.log('OFFSET', OFFSET);
-  console.log('1', x1, y1);
-  console.log('2', x2, y2);
-  console.log('3', x3, y3);
-  console.log('4', x4, y4);
-  console.groupEnd();
+  c.moveTo(0, FLOOR + offset);
+  c.lineTo(WIDTH, FLOOR + offset);
 
-  c.moveTo(x1, y1);
-  c.lineTo(x2, y2); // top
-  c.lineTo(x3, y3); // right
-  c.lineTo(x4, y4); // bottom
-  c.lineTo(x1, y1); // left
+  // lineAtAngle(0, HEIGHT - (distance * size), WIDTH, 0);
+  // The horizontal lines get closer as we get closer to the vanishing point.
+  // Use a vanishing point line to calculate the offset.
+  // const angle = 90 * Math.PI / 180;
+  // const length = size;
+  //
+  // const x1 = VPOINT.X;
+  // const y1 = 275;
+  // const x2 = x1 + length * Math.cos(angle);
+  // const y2 = y1 + length * Math.sin(angle);
+  // c.moveTo(x1, y1);
+  // c.lineTo(x2, y2);
 
-  // c.moveTo(x, y);
-  // c.lineTo(x + width, y); // top
-  // c.lineTo(x + width, y + height); // right
-  // c.lineTo(x, y + height); // bottom
-  // c.lineTo(x, y); // left
   c.closePath();
   c.stroke();
-  c.fill();
 }
